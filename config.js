4185b2b171511c9328615432e775b691bc9e1a81
HASHU-MD <dinujavirajitha364@gmail.com>
2026-04-05 15:14:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['💋', '🍬', '🫆', '💗', '🎈', '🎉', '🥳', '❤️', '🧫', '🐭'],
+    PREFIX: '.',
+	  BOT_NAME: 'DEATH-NOTE-MD',
+    MAX_RETRIES: 3,
+    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/LEHtkTJK49VJ3qtAUdGDnH?mode=wwt',
+    ADMIN_LIST_PATH: './admin.json',
+    RCD_IMAGE_PATH: './IMG-20251024-WA0009.jpg',
+    NEWSLETTER_JID: '120363395674230271@newsletter',
+    NEWSLETTER_MESSAGE_ID: '428',
+    OTP_EXPIRY: 300000,
+    OWNER_NUMBER: '94765725271',
+    CHANNEL_LINK: 'https://whatsapp.com/channel/0029VazhnLzK0IBdwXG4152o'
+};
